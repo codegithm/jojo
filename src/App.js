3e0ad3624f0b15fc343c8bf96d8434179f4e9903
Andile <xakazantando@gmail.com>
2021-10-11 16:06:57 +0200

    Picture resizing

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "../node_modules/bootstrap/dist/js/bootstrap.bundle";
 import Nav from "./Components/Nav/Nav";
 import Home from "./Components/Home/Home";
+import Footer from "./Components/Footer/Footer";
 
 function App() {
   return (
     <div className="App">
       <Nav />
       <Home />
+      <Footer />
     </div>
   );
 }
---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./Footer.css";
+const Footer = () => {
+  return (
+    <div className="footer">
+      <div className="contact">
+        <h4>Contact Details</h4>
+        <ul>
+          <li>
+            Cellphone :<a phone="0788255458">078 825 5458 </a>
+          </li>
+          <li>
+            <a href="jojoproductionsj@gmail.com">jojoproductionsj@gmail.com</a>
+          </li>
+          <li>P.O Box 619</li>
+          <li>Ulundi</li>
+          <li>© 2019 Jojo Productions</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
